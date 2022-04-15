e34708dafb125bfa0edc59cbf14a5cee20d4a5bf
zpershuai <zpershuai@gmail.com>
2022-04-16 01:16:36 +0800

    add .lock

--- spacemacs-develop/elpa/27.2/develop/fuzzy-20211231.1837/fuzzy-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from fuzzy.el  -*- no-byte-compile: t -*-
-(define-package "fuzzy" "20211231.1837" "Fuzzy Matching" '((emacs "24.3")) :commit "20a76509831b8296bf3c8b389e1f5602c2a88899" :authors '(("Tomohiro Matsuyama" . "m2ym.pub@gmail.com")) :maintainer '("Tomohiro Matsuyama" . "m2ym.pub@gmail.com") :keywords '("convenience") :url "https://github.com/auto-complete/fuzzy-el")
